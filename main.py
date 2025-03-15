4231bb9c8580f5c592291e5630f57267f0d1ce93
Leon Lonsdale <leon.codes@icloud.com>
2025-03-15 16:23:27 +0000

    update for newly added lessons

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-def main():
-    book_path = "books/frankenstein.txt"
+import sys
+
+from pathlib import Path
+
+from stats import get_word_count
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     words = get_word_count(text)
     characters = get_characters(text)
@@ -8,35 +19,46 @@ def main():
     print(f"{words} words found in the document")
     print()
     for character in characters_sorted:
-        print(f"'{character['char']}' appeared '{character['count']}' times")
+        print(f"{character['char']}: {character['count']}")
     print("--- End report ---")
 
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+def get_book_text(path: str) -> str:
+    file_path = Path(path)
 
-def get_word_count(text):
-    return len(text.split())
+    try:
+        with file_path.open("r", encoding="utf-8") as f:
+            return f.read()
+    except FileNotFoundError:
+        print(f"Error: File not found at path: {file_path}")
+        return ""
+    except Exception as e:
+        print(f"An error occurred while reading the file: {e}")
+        return ""
 
-def get_characters(text):
+
+def get_characters(text: str) -> dict:
     lower = text.lower()
     chars = {}
 
     for char in lower:
         if char.isalpha():
-            chars[char] = chars.get(char,0) + 1
+            chars[char] = chars.get(char, 0) + 1
 
     return chars
 
-def sort_by(dict):
-    return dict["count"]
 
-def sort_dict_to_list(dict):
+def sort_by(dictionary: dict) -> int:
+    return dictionary["count"]
+
+
+def sort_dict_to_list(dictionary: dict) -> list:
     sorted_list = []
-    for item in dict:
-        sorted_list.append({ "char": item, "count": dict[item] })
-    sorted_list.sort(reverse = True, key = sort_by)
+    for item in dictionary:
+        sorted_list.append({"char": item, "count": dictionary[item]})
+    sorted_list.sort(reverse=True, key=sort_by)
     return sorted_list
 
-main()
+
+if __name__ == "__main__":
+    main()
